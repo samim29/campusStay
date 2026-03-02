0be9597c6ba806ef2558f82e9788fca271112c48
Samim29 <samimalisk000@gmail.com>
2026-03-02 23:46:07 +0530

    authMiddleware and rolebased middleware implemented

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    if (!authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Invalid token format" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id).select("-password");
+
+    if (!req.user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token invalid or expired" });
+  }
+};
+
+module.exports = protect;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {registerUser,loginUser} = require("../controllers/authController");
+//const protect = require("../middleware/authMiddleware");
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
-
+// router.get("/profile", protect, (req, res) => {
+//   res.json(req.user);
+// });
 module.exports = router;
