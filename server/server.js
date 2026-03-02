9a32e48dd3d16ca84f893f826b12310ce2dd19bf
Samim29 <samimalisk000@gmail.com>
2026-03-02 22:45:20 +0530

    Auth routes implemented with register and login functionalities

--- server/server.js
+++ server/server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+
 dotenv.config();
+
 const port = process.env.PORT || 8080;
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth",authRoutes);
+
+
 connectDB();
 app.listen(port,()=>{
     console.log(`server is listening to port ${port}`);
